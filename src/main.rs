15a5e3a52aff739452f802d154fc08bcf76c5795
Ricardo Lima <ricardo.hrlima@gmail.com>
2024-11-10 17:15:48 -0300

    style: formatting

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 use std::{error::Error, net::SocketAddr};
 
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
 
     let version = 3.0;
-	let app = Router::new().route("/", get(move || hello(version)));
+    let app = Router::new().route("/", get(move || hello(version)));
 
     println!("listening on {}", addr);
 
